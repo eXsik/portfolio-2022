de8530402c88f75f6ba97b97250f3f2d3e55b885
Kuba Skupinski <qba.skupinski@gmail.com>
2022-10-27 10:06:11 +0200

    finished, works section

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,26 +1,50 @@
-import React, {Component} from 'react';
+import React, { useState } from "react";
 import { images } from "../../constants";
-import  './Navbar.scss';
+import "./Navbar.scss";
+import { HiMenuAlt4, HiX } from 'react-icons/hi'; 
+import { motion } from 'framer-motion'; 
 
-class Navbar extends Component {
-  render() {
-    return (
-      <nav className="app__navbar">
+export default function Navbar() {
+  const [toggle, setToggle] = useState(false)
+
+  return (
+    <nav className="app__navbar">
         <div className="app__navbar-logo">
-            <img src={images.logo} alt="logo" />
+          <img src={images.logo} alt="logo" />
         </div>
         <ul className="app__navbar-links">
-          {['home', 'about', 'work', 'skills', 'contact'].map(item => (
+          {["home", "about", "work", "skills", "contact"].map((item) => (
             <li className="app__flex p-text" key={`link-${item}`}>
-              <div>
-                <a href={`#${item}`}>{item}</a>
-              </div>
+              <div></div>
+              <a href={`#${item}`} title={item}>
+                {item}
+              </a>
             </li>
           ))}
         </ul>
-      </nav>
-    );
-  }
-}
 
-export default Navbar;
+        <div className="app__navbar-menu">
+          <HiMenuAlt4 onClick={() => setToggle(true)}></HiMenuAlt4>
+
+          {toggle && (
+              <motion.div
+                whileInView={{ x: [300, 0] }}
+                transition={{duration: 0.85, ease: "easeOut" }} 
+              >
+                <HiX onClick={() => setToggle(false)}></HiX>
+                <ul>
+
+                  {["home", "about", "work", "skills", "contact"].map((item) => (
+                    <li key={item}>
+                      <a href={`#${item}`} title={item} onClick={() => setToggle(false)}>
+                        {item}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </motion.div>
+          )}
+        </div>
+      </nav>
+  )
+}
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,71 @@
-import React from 'react';
-import  './Header.scss';
+import React from "react";
+import { motion } from "framer-motion";
 
-const Header = (props) => {
-    return(
-        <div>Header</div>
-    )
+import { images } from '../../constants';
+import "./Header.scss";
+import { AppWrap } from "../../wrapper";
+
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: 'easeInOut'
+    }
+  }
+}
+
+const Header = () => {
+  return (
+    <div className="app__header app__flex">
+      <motion.div 
+        whileInView={{ x: [-100, 0], opacity: [0, 1]}}
+        transition={{ duration: 0.5 }}
+        className="app__header-info"
+      >
+        <div className="app__header-badge">
+          <div className="badge-cmp app_flex">
+            <div>
+              <p className="p-text"> Hello, I'm</p>
+              <h1 className="head-text">Micael </h1>
+            </div>
+          </div>
+          <div className="tag-cmp app_flex">
+            <p className="p-text">Web Developer</p>
+            <p className="p-text">Graphic designer</p>
+          </div>
+        </div>
+      </motion.div>
+
+      <motion.div
+        whileInView={{ opacity: [0, 1]}}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__header-img"
+      >
+        <img src={images.profile} alt="profile_bg" />
+        <motion.img
+          whileInView={{ scale: [0, 1]}}
+          transition={{ duration: 1, ease: 'easeInOut' }}
+          className="overlay_circle"
+          src={images.circle}
+          alt="profile_circle"
+    
+        />
+      </motion.div>
+
+      <motion.div variant={scaleVariants}
+        whileInView={scaleVariants.whileInView}
+        className="app__header-circles"
+      >
+        {[images.flutter, images.redux, images.sass].map((circle, index) => {
+          return <div className="circle-cmp app__flex" key={`circle-${index}`}>
+            <img src={circle} alt="circle" />
+          </div>
+        })}
+      </motion.div>
+    </div>
+  );
 };
 
-export default Header;
+export default AppWrap(Header, 'home');
